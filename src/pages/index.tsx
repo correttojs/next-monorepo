f7807d5e7c57a3c5fb3ff7e22d247faec074e471
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2021-11-06 19:55:30 +0100

    feat: airbnb gallery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Layout } from "../components/Layout/Layout";
 import { Map } from "../components/Map/Map";
 import { Links, Locale } from "../generated/codegen";
-
+import Image from "next/image";
 import { getAirbnbDetails } from "../server/pageProps/airbnb";
 import { pdp_listing_detail } from "../server/pageProps/airbnb.types";
 import { getPageProps, PageProps } from "../server/pageProps/getPageProps";
@@ -45,7 +45,25 @@ const Home: NextPage<InitialProps> = ({
         headline={apartment?.headline ?? ""}
         subHeadline={apartment?.subHeadline ?? ""}
       />
-      <div>{airbnb?.pdp_listing_detail?.sectioned_description?.summary}</div>
+      <div className="main">
+        {airbnb?.pdp_listing_detail?.sectioned_description?.summary}
+      </div>
+
+      <div className="main grid grid-cols-2 md:grid-cols-3 gap-2">
+        {airbnb?.pdp_listing_detail.photos.map((photo, index) => {
+          return (
+            <div className="py-4" key={`p${index}`}>
+              <Image
+                src={photo.xx_large ?? ""}
+                width={400}
+                height={400 / 1.33}
+                alt={photo.caption ?? ""}
+                objectFit="cover"
+              />
+            </div>
+          );
+        })}
+      </div>
 
       <PageSections page={page} sections={sections} />
       <Map
