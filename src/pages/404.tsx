1d77d736a229ee7896e339d9237615fe361ecea9
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2021-11-21 21:47:39 +0100

    feat: not found

---
+++ src/pages/404.tsx
@@ -0,0 +1,32 @@
+import { GetStaticProps, NextPage } from "next";
+import React from "react";
+import { Layout } from "../components/Layout/Layout";
+import { PageSections } from "../components/PageSections/PageSections";
+import { Links, Locale } from "../generated/codegen";
+import { initTranslations, useTranslations } from "../hooks/useTranslations";
+import { getPageProps, PageProps } from "../server/pageProps/getPageProps";
+
+export const getStaticProps: GetStaticProps<PageProps> = async (params) => {
+  const data = await getPageProps({
+    pageType: Links.Home,
+    locale: params.locale === "de" ? Locale.De : Locale.En,
+  });
+
+  return {
+    props: data,
+  };
+};
+
+const PageError: NextPage<PageProps> = ({ apartment, links, translations }) => {
+  initTranslations(translations);
+  const translate = useTranslations();
+  return (
+    <Layout apartment={apartment} links={links}>
+      <div className="py-20 h-96 main">
+        <h1 className="h1">{translate("PAGE_NOT_FOUND")}</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default PageError;
