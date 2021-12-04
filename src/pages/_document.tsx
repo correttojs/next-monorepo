bf69616a5225b6f5d6294764ed90331c45e4b97b
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2021-12-04 10:06:15 +0100

    fix: document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
+class MyDocument extends (Document as any) {
   render() {
     return (
       <Html>
