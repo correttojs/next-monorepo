4b972cdef28e2655cf14e304bd4204d65668c570
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2022-01-01 18:24:42 +0100

    fix: button

--- packages/atticodellino/src/components/@UI/Texts.tsx
+++ packages/atticodellino/src/components/@UI/Texts.tsx
@@ -1,7 +1,68 @@
-import tw from "twin.macro";
+import React from "react";
 
-export const H1 = tw.h1`font-semibold text-5xl leading-10 pt-2`;
-export const H2 = tw.h2`font-semibold text-3xl leading-8`;
-export const H3 = tw.h3`font-semibold text-xl leading-6 `;
-export const P = tw.p`text-xl leading-8`;
-export const Span = tw.span`text-lg`;
+export const H1: React.FC<
+  React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLHeadingElement>,
+    HTMLHeadingElement
+  >
+> = ({ children, className, ...props }) => (
+  <h1
+    className={`font-semibold text-5xl leading-10 pt-2  ${className ?? ""}`}
+    {...props}
+  >
+    {children}
+  </h1>
+);
+
+export const H2: React.FC<
+  React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLHeadingElement>,
+    HTMLHeadingElement
+  >
+> = ({ children, className, ...props }) => (
+  <h2
+    css={`
+      font-size: 3rem;
+      line-height: 3rem;
+    `}
+    className={`font-semibold text-3xl leading-8  ${className ?? ""}`}
+    {...props}
+  >
+    {children}
+  </h2>
+);
+export const H3: React.FC<
+  React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLHeadingElement>,
+    HTMLHeadingElement
+  >
+> = ({ children, className, ...props }) => (
+  <h3
+    className={`font-semibold text-xl leading-6 ${className ?? ""}`}
+    {...props}
+  >
+    {children}
+  </h3>
+);
+
+export const P: React.FC<
+  React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLParagraphElement>,
+    HTMLParagraphElement
+  >
+> = ({ children, className, ...props }) => (
+  <p className={`text-xl leading-8 ${className ?? ""}`} {...props}>
+    {children}
+  </p>
+);
+
+export const Span: React.FC<
+  React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLSpanElement>,
+    HTMLSpanElement
+  >
+> = ({ children, className, ...props }) => (
+  <span className={`text-lg ${className ?? ""}`} {...props}>
+    {children}
+  </span>
+);
