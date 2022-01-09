2641c264e3ef14b5aad1d3655a12a505124b8b55
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2022-01-09 11:29:20 +0100

    feat: sb interactions

---
+++ packages/ui/src/Button/ButtonBase.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable no-nested-ternary */
+import classNames from "classnames";
+import React from "react";
+import {
+  TwColorClass,
+  TwColorHoverClass,
+  TwMargin,
+} from "@packages/ui/tailwind.types";
+
+export const Button: React.FC<
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > & {
+    colorClasses: TwColorClass[];
+    hoverColorClasses: TwColorHoverClass[];
+    marginClasses: TwMargin[];
+    size?: "S" | "M";
+    Icon?: React.ReactElement;
+    isRounded?: boolean;
+  }
+> = ({
+  className,
+  children,
+  size = "M",
+  Icon,
+  color,
+  colorClasses,
+  hoverColorClasses,
+  marginClasses,
+  isRounded = true,
+  ...props
+}) => {
+  return (
+    <button
+      className={classNames(
+        ...colorClasses,
+        ...hoverColorClasses,
+        ...marginClasses,
+        className,
+        isRounded && "rounded",
+        "hover:text-white tracking-wider py-2",
+        size === "M" ? "px-6" : "px-2",
+        `border text-white `
+      )}
+      {...props}
+    >
+      {Icon ? (
+        <div className="flex items-center">
+          {Icon} <span className="mx-2">{children}</span>
+        </div>
+      ) : (
+        children
+      )}
+    </button>
+  );
+};
--- packages/ui/src/Button/__stories__/Button.stories.tsx
+++ packages/ui/src/Button/__stories__/Button.stories.tsx
@@ -1,9 +1,11 @@
+import { expect } from "@storybook/jest";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Meta, Story } from "@storybook/react/types-6-0";
 import React from "react";
-import { Button } from "../Button";
+import { Button } from "../ButtonBase";
 import { MdSync } from "react-icons/md";
 import { ButtonSquare } from "@packages/ui/Button/ButtonSquare";
+import { screen, userEvent } from "@storybook/testing-library";
 
 export default {
   title: "UI/Button",
@@ -11,16 +13,46 @@ export default {
 
 export const Buttons: Story = (args) => (
   <div>
-    <Button className="m-4">Default</Button>
-    <Button className="m-4" isInverted={true}>
+    <Button
+      colorClasses={["bg-green-800", "border-blue-800"]}
+      hoverColorClasses={["hover:border-white", "hover:text-amber-800"]}
+      marginClasses={["m-4"]}
+      onClick={() => {
+        console.log("hello");
+      }}
+    >
+      Default Button
+    </Button>
+    <Button
+      colorClasses={["bg-green-800", "border-blue-800"]}
+      hoverColorClasses={["hover:border-cyan-800", "hover:text-amber-800"]}
+      marginClasses={["m-4"]}
+    >
       Inverted
     </Button>
-    <Button className="m-4" size={"S"}>
+    <Button
+      colorClasses={["bg-green-800", "border-blue-800"]}
+      hoverColorClasses={["hover:border-cyan-800", "hover:text-amber-800"]}
+      marginClasses={["m-4"]}
+      size={"S"}
+    >
       Small
     </Button>
-    <Button className="m-4" Icon={<MdSync />}>
+    <Button
+      colorClasses={["bg-green-800", "border-blue-800"]}
+      hoverColorClasses={["hover:border-cyan-800", "hover:text-amber-800"]}
+      marginClasses={["m-4"]}
+      Icon={<MdSync />}
+    >
       With Icon
     </Button>
     <ButtonSquare className="m-4">Square</ButtonSquare>
   </div>
 );
+
+Buttons.play = async () => {
+  const button = screen.getByText("Default Button");
+
+  await userEvent.click(button);
+  await expect(false).toBeTruthy();
+};
