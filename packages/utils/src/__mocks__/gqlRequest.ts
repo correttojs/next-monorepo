7df3dc5603dac29c2be9a022c3f3e36dbcc6cc30
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2022-07-26 13:02:15 +0200

    fix: tests

---
+++ packages/utils/src/__mocks__/gqlRequest.ts
@@ -0,0 +1,10 @@
+import type { TypedDocumentNode } from "@graphql-typed-document-node/core";
+import { GraphQLClient } from "graphql-request";
+
+export const gqlRequest = <TData, TVariables>(
+  document: TypedDocumentNode<TData, TVariables>,
+  variables?: TVariables,
+  url = `/api/graphql`
+) => {
+ return jest.fn(() => {console.log("mutate!"); return Promise.resolve()}); 
+}
