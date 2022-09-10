92ab3d4c8b410c1aed9c900fa3b6a592510abe3e
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2022-09-10 21:27:15 +0200

    tsc

--- packages/utils/src/gqlRequest.ts
+++ packages/utils/src/gqlRequest.ts
@@ -1,10 +1,11 @@
 import type { TypedDocumentNode } from "@graphql-typed-document-node/core";
-import { GraphQLClient } from "graphql-request";
+import { GraphQLClient, Variables } from "graphql-request";
 
-export const gqlRequest = <TData, TVariables>(
+export const gqlRequest = <TData, TVariables = Variables>(
   document: TypedDocumentNode<TData, TVariables>,
   variables?: TVariables,
   url = `/api/graphql`
 ) => {
-  console.log("READL")
- return  new GraphQLClient(url).request<TData, TVariables>(document, variables)};
+  console.log("READL");
+  return new GraphQLClient(url).request<TData>(document, variables as any);
+};
