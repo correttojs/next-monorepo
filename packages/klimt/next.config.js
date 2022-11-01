030038de8dcd84521afaa6980f2d3c38a534d743
Fabio Benedetti <fabio.benedetti@vercel.com>
2022-11-01 17:02:17 +0100

    fix transpile

--- packages/klimt/next.config.js
+++ packages/klimt/next.config.js
@@ -1,23 +1,17 @@
 /** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")([
-  "@packages/ui",
-  "@packages/utils",
-]);
+
 const withGraphql = require("next-plugin-graphql");
-module.exports = withTM(
-  withGraphql({
-    images: {
-      domains: [
-        "a0.muscache.com",
-        "media.graphcms.com",
-        "media.graphassets.com",
-      ],
-    },
-    i18n: {
-      locales: ["de", "en"],
-      defaultLocale: "de",
-    },
-    reactStrictMode: true,
-    swcMinify: true,
-  })
-);
+module.exports = withGraphql({
+  images: {
+    domains: ["a0.muscache.com", "media.graphcms.com", "media.graphassets.com"],
+  },
+  i18n: {
+    locales: ["de", "en"],
+    defaultLocale: "de",
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    transpilePackages: ["@packages/ui", "@packages/utils"],
+  },
+});
