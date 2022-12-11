019e16d7d7d6dfa75a63dd3238eb78e94aaf2958
Fabio Benedetti <fabio.benedetti@vercel.com>
2022-12-11 12:35:54 +0100

    test airbnb

--- packages/klimt/src/pages/index.tsx
+++ packages/klimt/src/pages/index.tsx
@@ -33,7 +33,7 @@ export const getStaticProps: GetStaticProps<InitialProps> = async (params) => {
 
 const Home: NextPage<InitialProps> = ({
   page,
-  airbnb,
+  // airbnb,
   links,
   sections,
   apartment,
@@ -41,32 +41,32 @@ const Home: NextPage<InitialProps> = ({
 }) => {
   initTranslations(translations);
   return null;
-  // return (
-  //   <Layout apartment={apartment} links={links} isTransparent={true}>
-  //     <Hero
-  //       headline={apartment?.headline ?? ""}
-  //       subHeadline={apartment?.subHeadline ?? ""}
-  //     />
-  //     <div className="main py-10">
-  //       <AnchorPointer id="home" />
-  //       <section className="">
-  //         {airbnb?.pdp_listing_detail?.sectioned_description?.summary}
-  //         {
-  //           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  //         }
-  //       </section>
-  //     </div>
-  //     <Gallery photos={airbnb?.pdp_listing_detail?.photos ?? []} />
+  return (
+    <Layout apartment={apartment} links={links} isTransparent={true}>
+      <Hero
+        headline={apartment?.headline ?? ""}
+        subHeadline={apartment?.subHeadline ?? ""}
+      />
+      <div className="main py-10">
+        <AnchorPointer id="home" />
+        {/* <section className="">
+          {airbnb?.pdp_listing_detail?.sectioned_description?.summary}
+          {
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }
+        </section> */}
+      </div>
+      {/* <Gallery photos={airbnb?.pdp_listing_detail?.photos ?? []} /> */}
 
-  //     <Contact apartment={apartment} />
-  //     <Map
-  //       title={apartment?.name ?? ""}
-  //       lat={apartment?.location?.latitude ?? 0}
-  //       lng={apartment?.location?.longitude ?? 0}
-  //       className="main h-map py-10"
-  //     />
-  //   </Layout>
-  // );
+      <Contact apartment={apartment} />
+      <Map
+        title={apartment?.name ?? ""}
+        lat={apartment?.location?.latitude ?? 0}
+        lng={apartment?.location?.longitude ?? 0}
+        className="main h-map py-10"
+      />
+    </Layout>
+  );
 };
 
 export default Home;
