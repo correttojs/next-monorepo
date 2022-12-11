6a462c2b9cb66966c818d8ffd095041d21e75510
Fabio Benedetti <fabio.benedetti@vercel.com>
2022-12-11 10:42:44 +0100

    redirect

--- packages/klimt/next.config.js
+++ packages/klimt/next.config.js
@@ -5,14 +5,20 @@ module.exports = withGraphql({
   images: {
     domains: ["a0.muscache.com", "media.graphcms.com", "media.graphassets.com"],
   },
-  // i18n: {
-  //   locales: ["de", "en"],
-  //   defaultLocale: "de",
-  // },
+
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     appDir: true,
     transpilePackages: ["@packages/ui", "@packages/utils"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: true,
+      },
+    ];
   },
 });
