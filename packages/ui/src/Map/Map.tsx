f1165535331658664dee2aff29090925913ed53b
Fabio Benedetti <fabio.benedetti@vercel.com>
2022-12-11 18:39:16 +0100

    map

--- packages/ui/src/Map/Map.tsx
+++ packages/ui/src/Map/Map.tsx
@@ -1,47 +1,111 @@
 "use client";
-import GoogleMapReact from "google-map-react";
-import React, { useState } from "react";
-import { FaMapMarker } from "react-icons/fa";
+import React from "react";
 import { useInView } from "react-intersection-observer";
+import { Wrapper } from "@googlemaps/react-wrapper";
 
-const GoogleMap =
-  GoogleMapReact as unknown as React.JSXElementConstructor<GoogleMapReact.Props>;
+type MapProps = {
+  title: string;
+  lat: number;
+  lng: number;
+};
 
-const NodeMarker: React.FC<React.PropsWithChildren<{ title: string }>> = ({
+const Marker: React.FC<google.maps.MarkerOptions & MapProps> = (options) => {
+  const [marker, setMarker] = React.useState<google.maps.Marker>();
+  const svgMarker = {
+    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+    fillColor: "black",
+    fillOpacity: 1,
+    strokeWeight: 0,
+    rotation: 0,
+    scale: 2,
+    position: options.position,
+  };
+  React.useEffect(() => {
+    if (!marker) {
+      setMarker(new google.maps.Marker());
+    }
+
+    // remove marker from map on unmount
+    return () => {
+      if (marker) {
+        marker.setMap(null);
+      }
+    };
+  }, [marker]);
+
+  React.useEffect(() => {
+    if (marker) {
+      marker.setOptions({
+        ...options,
+        label: {
+          text: options.title,
+          fontFamily: "Roboto",
+          fontSize: "14px",
+          fontWeight: "bold",
+        },
+        animation: google.maps.Animation.DROP,
+        icon: svgMarker,
+      });
+    }
+  }, [marker, options]);
+
+  return null;
+};
+
+const Map1: React.FC<React.PropsWithChildren<MapProps>> = ({
+  lat,
+  lng,
   title,
 }) => {
+  const ref = React.useRef(null);
+  const [map, setMap] = React.useState<google.maps.Map | null>(null);
+  React.useEffect(() => {
+    if (ref.current && !map) {
+      setMap(
+        new google.maps.Map(ref.current, {
+          zoomControl: true,
+          mapTypeControl: false,
+          streetViewControl: false,
+          center: {
+            lat: lat ?? 0,
+            lng: lng ?? 0,
+          },
+          zoom: 16,
+        })
+      );
+    }
+  }, [ref, map, lat, lng]);
+
   return (
-    <div>
-      <FaMapMarker /> <p>{title}</p>
-    </div>
+    <>
+      <div ref={ref} style={{ height: "100%", width: "100%" }} />
+      <Marker
+        title={title}
+        position={{ lat, lng }}
+        icon={"Lodging"}
+        map={map}
+        lat={lat}
+        lng={lng}
+      />
+    </>
   );
 };
 
 export const Map: React.FC<
-  React.PropsWithChildren<{
-    title: string;
-    lat: number;
-    lng: number;
-    className: string;
-  }>
+  React.PropsWithChildren<
+    MapProps & {
+      className: string;
+    }
+  >
 > = ({ title, lat, lng, className }) => {
-  const [isLoaded, setIsLoaded] = useState(false);
   const [ref, inView] = useInView({ triggerOnce: true });
-
   return (
-    // Important! Always set the container height explicitly
-    <div ref={ref} className={className} data-testid="google-map">
-      {inView && (
-        <GoogleMap
-          bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_MAP_KEY ?? "" }}
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          defaultCenter={[lat, lng] as any}
-          defaultZoom={16}
-          onGoogleApiLoaded={() => setIsLoaded(true)}
-        >
-          {isLoaded && <NodeMarker {...{ lat, lng }} title={title} />}
-        </GoogleMap>
-      )}
+    <div ref={ref} className={className}>
+      {inView ? (
+        <Wrapper apiKey={process.env.NEXT_PUBLIC_MAP_KEY ?? ""}>
+          <Map1 title={title} lat={lat} lng={lng} />
+        </Wrapper>
+      ) : null}
     </div>
   );
 };
--- packages/ui/src/Sections/MainSection.tsx
+++ packages/ui/src/Sections/MainSection.tsx
@@ -1,7 +1,14 @@
 import classNames from "classnames";
 import React from "react";
 
-export const MainSection: React.FC<React.PropsWithChildren<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>>> = ({ children, className, ...props }) => (
+export const MainSection: React.FC<
+  React.PropsWithChildren<
+    React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLDivElement>,
+      HTMLDivElement
+    >
+  >
+> = ({ children, className, ...props }) => (
   <section
     className={classNames(
       "container mx-auto max-w-screen-lg px-4 lg:px-0",
