71e4ad0b6f27c9ddc582dcb4e637a736f20ddf35
Fabio Benedetti <fabio.benedetti@vercel.com>
2023-03-04 17:17:03 +0100

    cleanup

--- packages/klimt/app/[locale]/_home/Contact/__tests__/Contacts.test.tsx
+++ packages/klimt/app/[locale]/_home/Contact/__tests__/Contacts.test.tsx
@@ -9,6 +9,7 @@ import { Contact } from "../Contact";
 jest.mock("../../../_layout/TranslationContext", () => ({
   useTranslations: () => (k: string) => k,
 }));
+
 jest.mock("@packages/utils/gqlRequest", () => ({
   ...jest.requireActual("@packages/utils/gqlRequest"),
   gqlRequest: jest.fn(),
@@ -21,27 +22,26 @@ describe("Contacts", () => {
 
   it("Should submit Contact form", async () => {
     render(<Contact apartment={{} as any} />);
-    await act(async () => {
-      await userEvent.type(screen.getByPlaceholderText(/INPUT_NAME/i), `John`);
-      await userEvent.type(
-        screen.getByPlaceholderText(/INPUT_EMAIL/i),
-        `test@email.com`
-      );
-      await userEvent.type(
-        screen.getByPlaceholderText(/INPUT_MESSAGE/i),
-        `message test`
-      );
 
-      await userEvent.click(screen.getByRole("button", { name: /SEND/i }));
+    await userEvent.type(screen.getByPlaceholderText(/INPUT_NAME/i), `John`);
+    await userEvent.type(
+      screen.getByPlaceholderText(/INPUT_EMAIL/i),
+      `test@email.com`
+    );
+    await userEvent.type(
+      screen.getByPlaceholderText(/INPUT_MESSAGE/i),
+      `message test`
+    );
 
-      await waitFor(() =>
-        expect(gqlRequest).toHaveBeenCalledWith(expect.anything(), {
-          email: "test@email.com",
-          name: "John",
-          message: "message test",
-        })
-      );
-    });
+    await userEvent.click(screen.getByRole("button", { name: /SEND/i }));
+
+    await waitFor(() =>
+      expect(gqlRequest).toHaveBeenCalledWith(expect.anything(), {
+        email: "test@email.com",
+        name: "John",
+        message: "message test",
+      })
+    );
   });
 
   it("Should NOT submit Contact form", async () => {
--- packages/klimt/src/resolvers/__tests__/resolvers.test.ts
+++
@@ -1,9 +0,0 @@
-import { resolvers } from "../resolvers";
-
-describe("Resolvers", () => {
-  it("Sample test", () => {
-    if (resolvers?.Query?.hello) {
-      expect(1).toBe(1);
-    }
-  });
-});
