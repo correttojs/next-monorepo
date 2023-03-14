3729b7486ce4d0cbe01d31d811d7fd2ad895c865
Fabio Benedetti <fabio.benedetti@vercel.com>
2023-03-14 19:52:56 +0100

    update flex

--- packages/ui/src/Flex/Flex.tsx
+++ packages/ui/src/Flex/Flex.tsx
@@ -31,5 +31,27 @@ export const Flex = classed.div("flex", {
       7: "gap-7",
       8: "gap-8",
     },
+    grow: {
+      0: "grow-0",
+      1: "grow",
+    },
+    flex: {
+      1: "flex-1",
+      auto: "flex-auto",
+      none: "flex-none",
+      initial: "flex-initial",
+    },
+    wrap: {
+      wrap: "flex-wrap",
+      "wrap-reverse": "flex-wrap-reverse",
+      "no-wrap": "flex-nowrap",
+    },
+    basis: {
+      "1/3": "basis-1/3",
+      "2/3": "basis-2/3",
+      "1/2": "basis-1/2",
+      "1/4": "basis-1/4",
+      "3/4": "basis-3/4",
+    },
   },
 });
